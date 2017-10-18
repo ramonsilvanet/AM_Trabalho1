82dde3a5779432a07e94e69f26269065c8edf4b5
Ramon Ferreira Silva <ramon.silva@m4u.com.br>
2017-10-18 19:53:35 -0200

    :imp: :construction: Gradiente Descendente regularizado para Regressão linear

--- aprendizado_maquina/regressao_linear.py
+++ aprendizado_maquina/regressao_linear.py
@@ -4,6 +4,10 @@ def custo_regressao_linear(X, y, theta):
     inner = np.power(((X * theta.T) - y), 2)
     return np.sum(inner) / (2 * len(X))
 
+def custo_regressao_linear_regularizada(theta, X, y, reg):
+    m = y.size
+    return custo_regressao_linear(X, y, theta) +  (reg / (2 * m)) * np.sum(np.square(theta[1:]))
+
 def gradiente_descendente_batch(X, y, theta, alpha, iters):
     temp = np.matrix(np.zeros(theta.shape))
     parameters = int(theta.ravel().shape[1])
